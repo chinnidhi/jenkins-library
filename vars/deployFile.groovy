d79e94d7219a363010c0e3902722b98f111eba44
chinnidhi <64729180+chinnidhi@users.noreply.github.com>
2025-02-06 20:16:06 +0530

    Update deployFile.groovy

--- vars/deployFile.groovy
+++ vars/deployFile.groovy
@@ -1,5 +1,5 @@
 def call(String BUILD_NUMBER, String IP_ADDRESS) {
-sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${IP_ADDRESS}:8082/artifactory/hello-world-war-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war"'
+sh '''curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${IP_ADDRESS}:8082/artifactory/hello-world-war-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war"'''
       sh 'sudo cp hello-world-war-${BUILD_NUMBER}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/'
      sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
      sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/startup.sh'
